2b309c6ddb0517cdbdeee771107ef9fbd496a3bd
Mohammed Umar Tanwar <95756609+UmarTanwar3527@users.noreply.github.com>
2022-10-15 20:59:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify, render_template
+# from flask_ngrok import run_with_ngrok
 import numpy as np
 import pickle
 
+
 app = Flask(__name__)
+# run_with_ngrok(app)
+
     
 model_RF=pickle.load(open('Major_RF.pkl', 'rb')) 
 model_KNN=pickle.load(open('Major_KNN.pkl', 'rb')) 
@@ -41,33 +45,37 @@ def minor():
 @app.route('/predict',methods=['GET'])
 
 def predict():
-  pH = float(request.args.get('pH'))
-  Temprature = float(request.args.get('Temprature'))
-  Fat = float(request.args.get('Fat'))
-  Turbidity = float(request.args.get('Turbidity'))
     
+     
+    pH = float(request.args.get('pH'))
+    Temprature = float(request.args.get('Temprature'))
+    Taste = float(request.args.get('Taste'))
+    Odor = float(request.args.get('Odor'))
+    Fat = float(request.args.get('Fat'))
+    Turbidity = float(request.args.get('Turbidity'))
+    Colour = float(request.args.get('Colour'))
 
  
     Model = (request.args.get('Model'))
 
     if Model=="Random Forest Classifier":
-      prediction = model_RF.predict([[pH,Temprature,Fat,Turbidity]])
+      prediction = model_RF.predict([[pH, Temprature, Taste, Odor, Fat, Turbidity, Colour]])
       accr="99.62%"
 
     elif Model=="Decision Tree Classifier":
-      prediction = model_DT.predict([[pH,Temprature,Fat,Turbidity]])
+      prediction = model_DT.predict([[pH, Temprature, Taste, Odor, Fat, Turbidity, Colour]])
       accr="98.87%"
 
     elif Model=="KNN Classifier":
-      prediction = model_KNN.predict([[pH,Temprature,Fat,Turbidity]])
+      prediction = model_KNN.predict([[pH, Temprature, Taste, Odor, Fat, Turbidity, Colour]])
       accr="99.25%"
 
     elif Model=="SVM Classifier":
-      prediction = model_K_SVM.predict([[pH,Temprature,Fat,Turbidity]])
+      prediction = model_K_SVM.predict([[pH, Temprature, Taste, Odor, Fat, Turbidity, Colour]])
       accr="85.28%"
 
     else:
-      prediction = model_NB.predict([[pH,Temprature,Fat,Turbidity]])
+      prediction = model_NB.predict([[pH, Temprature, Taste, Odor, Fat, Turbidity, Colour]])
       accr="92.08%"
 
     
